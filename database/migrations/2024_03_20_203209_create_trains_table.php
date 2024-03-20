9d96d2eb8dc9bf093fc3820312e8444dc1a672f6
Christian-Lepidi <christianlepidi@gmail.com>
2024-03-20 22:05:32 +0100

    feat: add. table contents

--- database/migrations/2024_03_20_203209_create_trains_table.php
+++ database/migrations/2024_03_20_203209_create_trains_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 50);
+            $table->string('stazione_partenza', 50);
+            $table->time('orario_partenza');
+            $table->time('orario_arrivo');
+            $table->string('stazione_arrivo', 50);
+            $table->string('codice_treno', 6);
+            $table->char('numero_carrozze', 30);
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
